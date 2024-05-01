8436e1319b0ec71f031dcd37aa357c0fa744c474
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-01 18:29:02 -0300

    feat(mobile): add NavigateContainer

--- money-mobile/App.tsx
+++ money-mobile/App.tsx
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { Routes } from "./src/routes";
 
 function App() {
-  return <Text>Teste!!!</Text>;
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
 }
 
 export default App;
