c3821826701afba1062b20e4dca259295e65c2b9
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-01 15:03:24 -0300

    refactor(mobile): add useGetExpenses

--- money-mobile/src/components/header/index.tsx
+++ money-mobile/src/components/header/index.tsx
@@ -1,13 +1,15 @@
 import { Image, View } from "react-native";
 import icons from "../../constants/icons";
+import { useGetExpenses } from "../../hooks/useGetExpenses";
 import { PrimaryCard } from "../primaryCard";
 import { styles } from "./style";
 
 function Header() {
+  const { totalExpenses } = useGetExpenses();
   return (
     <View style={styles.container}>
       <Image style={styles.logo} source={icons.logo} />
-      <PrimaryCard />
+      <PrimaryCard value={totalExpenses} />
     </View>
   );
 }
