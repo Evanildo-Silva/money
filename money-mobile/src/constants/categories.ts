0895820db7019ef2f3f988346261acc4a32c89a0
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-01 17:16:41 -0300

    feat(mobile): create categories constants

---
+++ money-mobile/src/constants/categories.ts
@@ -0,0 +1,10 @@
+const CATEGORIES = [
+  { id: "Carro", value: "Carro" },
+  { id: "Casa", value: "Casa" },
+  { id: "Lazer", value: "Lazer" },
+  { id: "Mercado", value: "Mercado" },
+  { id: "Educação", value: "Educação" },
+  { id: "Viagem", value: "Viagem" },
+];
+
+export { CATEGORIES };
