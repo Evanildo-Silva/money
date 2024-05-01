98d8bacb6e528ed50aaf72e84c7a4870d944d519
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-04-30 23:26:51 -0300

    feat(mobile): create icons file for import images

---
+++ money-mobile/src/constants/icons.ts
@@ -0,0 +1,21 @@
+import airplane from "../assets/icon-airplane.svg";
+import bike from "../assets/icon-bike.svg";
+import book from "../assets/icon-book.svg";
+import car from "../assets/icon-car.svg";
+import dollar from "../assets/icon-dollar-sign.svg";
+import house from "../assets/icon-house.svg";
+import logo from "../assets/icon-logo.svg";
+import marketplaceCart from "../assets/icon-marketplace-cart.svg";
+import trash from "../assets/icon-trash.svg";
+
+export default {
+  airplane,
+  bike,
+  book,
+  car,
+  dollar,
+  house,
+  marketplaceCart,
+  trash,
+  logo,
+};
