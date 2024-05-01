df57d469b2b7db8965ac2e8ae2cf496c53455709
Evanildo-Silva <evanildo.sf91@gmail.com>
2024-05-01 11:53:00 -0300

    refactor(mobile): add styles

--- money-mobile/src/components/secondaryCard/style.ts
+++ money-mobile/src/components/secondaryCard/style.ts
@@ -1,5 +1,5 @@
 import { StyleSheet } from "react-native";
-import { COLORS } from "../../constants/theme";
+import { COLORS, FONT_SIZE } from "../../constants/theme";
 
 const styles = StyleSheet.create({
   container: {
@@ -9,6 +9,36 @@ const styles = StyleSheet.create({
     marginTop: 10,
     borderRadius: 10,
     flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  containerIcon: {
+    flex: 3,
+    justifyContent: "center",
+  },
+  icon: {
+    width: 35,
+    height: 35,
+  },
+  containerDescription: {
+    flex: 10,
+  },
+  title: {
+    fontSize: FONT_SIZE.sm,
+    fontWeight: "bold",
+    color: COLORS.dark_gray,
+  },
+  description: {
+    fontSize: FONT_SIZE.sm,
+    color: COLORS.dark_gray,
+  },
+  containerValue: {
+    flex: 4,
+  },
+  value: {
+    fontSize: FONT_SIZE.sm,
+    fontWeight: "bold",
+    color: COLORS.dark_gray,
+    textAlign: "right",
   },
 });
 
